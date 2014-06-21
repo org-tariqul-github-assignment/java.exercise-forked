79deb24f557b544e3508a0ca0d4fd1e1f98720a9
ISLAM <ISLAM@Tariqul-PC>
2014-06-21 15:47:26 +0900

    Reverse of a string

--- JavaExercise/src/tariqul/java/exercise/Main.java
+++ JavaExercise/src/tariqul/java/exercise/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(Utilities.reverseString("abcd"));
+		System.out.println(Utilities.reverseString2("abcd"));
+		System.out.println(Utilities.reverseString3("abcde"));
 	}
 
 }
