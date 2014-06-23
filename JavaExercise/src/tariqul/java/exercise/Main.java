25682bc5bb22c4826a4c5506fef41ee09a642e53
tariqul- <tariqulbd@gmail.com>
2014-06-23 18:44:53 +0900

    BST implementation

--- JavaExercise/src/tariqul/java/exercise/Main.java
+++ JavaExercise/src/tariqul/java/exercise/Main.java
@@ -11,12 +11,26 @@ public class Main {
 		*/
 		
 		// Circular queue
+		/*
 		CircularQueue cqueue = new CircularQueue(10);
 		cqueue.insert("A");
 		cqueue.insert("B");
 		System.out.println(cqueue.remove());
 		System.out.println(cqueue.remove());
 		System.out.println(cqueue.remove());
+		*/
+		
+		
+		//BST Insert and In Order Traverse
+		BinarySearchTree bst = new BinarySearchTree();
+		bst.insert(5);
+		bst.insert(1);
+		bst.insert(2);
+		bst.insert(10);
+		bst.insert(20);
+		bst.insert(7);
+		bst.inorder();
+		
 	}
 
 }
